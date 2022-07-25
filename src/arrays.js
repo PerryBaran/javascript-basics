e7229d458500f7ecb640dc4d59f21b4d97fd9f3f
Perry <petreli1@ymail.com>
2022-07-25 21:00:26 +0100

    completed array tests

--- src/arrays.js
+++ src/arrays.js
@@ -1,61 +1,130 @@
 const getNthElement = (index, array) => {
-  // your code here
+  let i = index;
+  const length = array.length - 1;
+  while (i > length) {
+    i -= array.length;
+  }
+  return array[i];
 };
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.toString();
 };
 
 const csvStringToArray = string => {
-  // your code here
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  const arrayClone = [...array];
+  arrayClone.push(element);
+  return arrayClone;
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+  const strings = [];
+  const { length } = numbers;
+  for (let i = 0; i < length; i += 1) {
+    strings.push(numbers[i].toString());
+  }
+  return strings;
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  const upperCase = [];
+  const { length } = strings;
+  for (let i = 0; i < length; i += 1) {
+    upperCase.push(strings[i].toUpperCase());
+  }
+  return upperCase;
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
+  const reversed = [];
+  const stringsLength = strings.length;
+  for (let i = 0; i < stringsLength; i += 1) {
+    const string = strings[i].split('');
+    const reversedString = [];
+    for (let j = string.length - 1; j >= 0; j -= 1) {
+      reversedString.push(string[j]);
+    }
+    reversed.push(reversedString.join(''));
+  }
+  return reversed;
 };
 
 const onlyEven = numbers => {
-  // your code here
+  const evenNumbers = [];
+  const { length } = numbers;
+  for (let i = 0; i < length; i += 1) {
+    const number = numbers[i];
+    if (number % 2 === 0) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  const newArray = [...array];
+  newArray.splice(index, 1);
+  return newArray;
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+  const vowelArray = [];
+  const { length } = strings;
+  for (let i = 0; i < length; i += 1) {
+    const string = strings[i];
+    const firstLetter = string.charAt(0).toLowerCase();
+    if (
+      firstLetter === 'a' ||
+      firstLetter === 'e' ||
+      firstLetter === 'i' ||
+      firstLetter === 'o' ||
+      firstLetter === 'u'
+    ) {
+      vowelArray.push(string);
+    }
+  }
+  return vowelArray;
 };
 
 const removeSpaces = string => {
-  // your code here
+  return string.replace(/ /g, '');
 };
 
 const sumNumbers = numbers => {
-  // your code here
+  let sum = 0;
+  numbers.forEach(number => {
+    sum += number;
+  });
+  return sum;
 };
 
 const sortByLastLetter = strings => {
-  // your code here
+  const reverseStrings = array => {
+    const reversedArray = [];
+    const { length } = array;
+    for (let i = 0; i < length; i += 1) {
+      const stringArray = array[i].split('');
+      stringArray.reverse();
+      const reversedString = stringArray.join('');
+      reversedArray.push(reversedString);
+    }
+    return reversedArray;
+  };
+  const reversedStrings = reverseStrings(strings);
+  reversedStrings.sort();
+  return reverseStrings(reversedStrings);
 };
 
 module.exports = {
