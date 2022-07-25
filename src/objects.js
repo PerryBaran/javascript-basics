19677255d3376cedfef5876e48ee19127c600edd
Perry <petreli1@ymail.com>
2022-07-25 21:01:53 +0100

    completed objects tests

--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,77 @@
 const createPerson = (name, age) => {
-  // your code here
+  return {
+    name,
+    age
+  };
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  if (object[property]) {
+    return true;
+  }
+  return false;
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age > 65) {
+    return true;
+  }
+  return false;
 };
 
 const getAges = people => {
-  // your code here
+  const ageArray = [];
+  people.forEach(person => {
+    ageArray.push(person.age);
+  });
+  return ageArray;
 };
 
 const findByName = (name, people) => {
-  // your code here
+  const { length } = people;
+  for (let i = 0; i < length; i += 1) {
+    const person = people[i];
+    if (person.name === name) {
+      return person;
+    }
+  }
+  return undefined;
 };
 
 const findHondas = cars => {
-  // your code here
+  const hondas = [];
+  cars.forEach(car => {
+    if (car.manufacturer === 'Honda') {
+      hondas.push(car);
+    }
+  });
+  return hondas;
 };
 
 const averageAge = people => {
-  // your code here
+  let totalAge = 0;
+  people.forEach(person => {
+    totalAge += person.age;
+  });
+  return Math.round(totalAge / people.length);
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  const introduce = person =>
+    `Hi ${person}, my name is ${name} and I am ${age}!`;
+  return {
+    name,
+    age,
+    introduce
+  };
 };
 
 module.exports = {
